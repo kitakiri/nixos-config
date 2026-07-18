fd183c6fc56796d244c6348f718ca4ec08bdd6bf
kitakiri <redjulius8@gmail.com>
2026-07-19 04:43:43 +0500

    пытаюсь сменить sddm на plm

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -6,18 +6,12 @@
 
   services.desktopManager.plasma6.enable = true;
 
+  services.displayManager.plasma-login-manager.enable = true;
 
 
+  # services.displayManager.sddm = {
+  #   enable = true;
+  #   wayland.enable = true;
+  # };
 
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-
-    theme = "breeze";
-  };
-
-  environment.etc."sddm/themes/breeze/theme.conf.user".text = ''
-    [General]
-    background=${../assets/wallhaven-8o2lm2_6000x3375.png}
-  '';
 }
