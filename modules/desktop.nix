9512d817a6be826a32dc044a9728e98448d1913d
kitakiri <redjulius8@gmail.com>
2026-07-19 05:29:38 +0500

    крч лучше через gui картинку ставить

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,17 +3,5 @@
 {
   services.desktopManager.plasma6.enable = true;
 
-  services.displayManager.plasma-login-manager = {
-    enable = true;
-
-    settings = {
-      "Greeter" = {
-        PreselectedUser = "kitakiri";
-      };
-
-      "Greeter][Wallpaper][org.kde.image][General" = {
-        Image = "file://${../assets/wallhaven-8o2lm2_6000x3375.png}";
-      };
-    };
-  };
+  services.displayManager.plasma-login-manager.enable = true;
 }
