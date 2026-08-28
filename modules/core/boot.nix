6ce27688f5d3a7de4f5bb933ed7d1a02e05f0216
kitakiri <redjulius8@gmail.com>
2026-08-29 00:43:04 +0500

    пытаюсь решить проблему со сном на десктопе

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -4,4 +4,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Должно помочь с тем чтобы usb устройства на desktop пробуждали пк из сна
+  # (естественно нужно и в uefi выключить ErP Ready и включить пробуждение по usb)
+  services.udev.extraRules = ''
+    # Автоматическое включение пробуждения для всех USB-устройств
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{power/wakeup}="enabled"
+  '';
 }
