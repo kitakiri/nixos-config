d7a0002bbf37e1a4a873a06a0cd5fb742fd33a4c
kitakiri <redjulius8@gmail.com>
2026-09-02 11:25:29 +0500

    поставил firefox браузером по умолчанию (не будет конфликтовать с zen из flatpak)

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -1,4 +1,3 @@
-# { pkgs, ... }:
 {
   pkgs,
   inputs,
@@ -23,6 +22,18 @@
           pywalfox
         ];
       };
+    };
+  };
+
+  # Ассоциации MIME-типов для Firefox
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
     };
   };
 }
